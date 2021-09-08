2a938d5ddacfc979cb334229064202c8bcddbfb4
usr_bazan <henrique.bazan86@gamil.com>
2021-09-08 16:55:06 -0300

    plugin aux do form

---
+++ wp-content/plugins/grumft-contact-form-plugin/assets/script-form-gmft.js
@@ -0,0 +1,112 @@
+var $ = jQuery.noConflict();
+jQuery(document).ready(function () {
+    const showLgpd = localStorage.getItem('showLgpd');
+    if (showLgpd != 'false') {
+        jQuery("#privacy-pop-up").css({
+            "display": "flex"
+        });
+    }
+    $('#exit-popup').click(function () {
+        jQuery("#privacy-pop-up").css({
+            "display": "none"
+        });
+        localStorage.setItem('showLgpd', 'false');
+    });
+});
+
+
+// $(".buttons_lp").click(function () {
+//     $("#click_origin").val($(this).attr("data-id"));
+//     $('#top-banner-form-inner')[0].scrollIntoView(true);
+// });
+
+jQuery("#btn-submit").click(function () {
+    var name = jQuery("#name").val();
+    var email = jQuery("#email").val();
+    var phone = jQuery("#phone").val();
+    var company = jQuery("#company").val();
+    var investing = jQuery("#investing").val();
+    var click_origin = jQuery("#click_origin").val();
+
+    var isValid = validateFields(name, email, phone, company, investing);
+    if (isValid == 4) {
+
+        jQuery("#btn-submit").prop("disabled", true);
+
+        var formData = {
+            'name': name,
+            'email': email,
+            'phone': phone,
+            'company': company,
+            'investing': investing,
+            'click_origin': click_origin
+        };
+
+        jQuery.ajax({
+            url: "/wp-json/register-form/send",
+            type: "POST",
+            data: formData,
+            success: function (data) {
+                /*var status = JSON.parse(data).status*/
+                if (data.status == 201) {
+                    jQuery("#click_origin").before('<div style="background-color: green;color: white;text-align: center;font-family: \'fonte-regular\';font-size: 13px;padding: 8px;border-radius: 9px;">ENTRAREMOS EM CONTATO O MAIS BREVE POSSÃVEL :)</div>');
+                } else {
+                    jQuery("#click_origin").before('<div style="background-color:#F52929;color:white;text-align:center;font-family:\'fonte-regular\';">OCORREU UM ERRO TENTE NOVAMENTE</div>');
+                }
+                jQuery("#btn-submit").prop("disabled", false);
+                clearFileds();
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                jQuery("#click_origin").before('<div style="background-color:#F52929;color:white;text-align:center;font-family:\'fonte-regular\';">OCORREU UM ERRO TENTE NOVAMENTE</div>');
+            }
+        });
+    }
+});
+
+function validateFields(name, email, phone, company, investing) {
+    var valid = 4;
+
+    if (name == "") {
+        jQuery("#name").css('border', 'solid red 3px');
+        valid--;
+    } else {
+        jQuery("#name").css('border', 'none');
+    }
+
+    if (email == "") {
+        jQuery("#email").css('border', 'solid red 3px');
+        valid--;
+    } else {
+        jQuery("#email").css('border', 'none');
+    }
+
+    if (phone == "") {
+        jQuery("#phone").css('border', 'solid red 3px');
+        valid--;
+    } else {
+        jQuery("#phone").css('border', 'none');
+    }
+
+    if (company == "") {
+        jQuery("#company").css('border', 'solid red 3px');
+        valid--;
+    } else {
+        jQuery("#company").css('border', 'none');
+    }
+
+    if (investing == "" || investing.includes("Média")) {
+        jQuery("#investing").css('border', 'solid red 3px');
+        valid--;
+    } else {
+        jQuery("#investing").css('border', 'none');
+    }
+
+    return valid;
+}
+
+function clearFileds() {
+    jQuery("#name").val("");
+    jQuery("#email").val("");
+    jQuery("#phone").val("");
+    jQuery("#company").val("");
+}
---
+++ wp-content/plugins/grumft-contact-form-plugin/core.php
@@ -0,0 +1,46 @@
+<?php
+/*
+	 * Plugin Name: Grumft Contact Form Plugin
+	 * Description: Grumft Contact Form Plugin.
+	 * Author: Grumft
+	 * Author URI: https://grumft.com
+	 * Version: 1.0
+	 */
+
+function send_register_form()
+{
+
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$phone = $_POST["phone"];
+	$company = $_POST["company"];
+	$click_origin = $_POST["click_origin"];
+	$apiKey = "1234";
+
+	$json_obj = json_encode(array("name" => $name, "email" => $email, "phone" => $phone, "company" => $company, 'click_origin' => $click_origin, 'investment' => '', 'apiKey' => $apiKey));
+
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, "https://admin.meediaonne.com/api/landing-page/store");
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_USERAGENT, 'Mediapartner-SeaStorm-1.0');
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $json_obj);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$server_output = curl_exec($ch);
+	curl_close($ch);
+
+	echo $server_output;
+}
+
+
+
+function prefix_register_send_form()
+{
+	register_rest_route('register-form', '/send', array(
+		'methods'  => 'POST',
+		'callback' => 'send_register_form',
+	));
+}
+
+add_action('rest_api_init', 'prefix_register_send_form');
