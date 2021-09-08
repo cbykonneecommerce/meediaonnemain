1d1d1ee84e3db794afac67975223b08c015a6048
usr_bazan <henrique.bazan86@gamil.com>
2021-09-08 18:06:11 -0300

    alteração para subscribe email

--- wp-content/plugins/grumft-contact-form-plugin/assets/script-form-gmft.js
+++ wp-content/plugins/grumft-contact-form-plugin/assets/script-form-gmft.js
@@ -105,9 +105,50 @@ function validateFields(name, email, phone, company, investment) {
 }
 
 function clearFileds() {
-    jQuery("#name").val("");
-    jQuery("#email").val("");
-    jQuery("#phone").val("");
-    jQuery("#company").val("");
-    jQuery("#investment").val("");
+    jQuery("#email_subscriber").val("");
 }
+
+jQuery("#btn-submit-subscriber").click(function () {
+    
+    var email = jQuery("#email_subscriber").val();    
+    var click_origin = jQuery("#click_origin_subscriber").val();
+
+    var isValid = false;
+    if (email == "") {
+        isValid = false;
+        jQuery("#email_subscriber").css('border', 'solid red 3px');
+        valid--;
+    } else {
+        isValid = true;
+        jQuery("#email_subscriber").css('border', 'none');
+    }
+
+    if (isValid) {
+
+        jQuery("#btn-submit-subscriber").prop("disabled", true);
+
+        var formData = {            
+            'email': email,            
+            'click_origin': click_origin
+        };
+
+        jQuery.ajax({
+            url: "/wp-json/email-subscriber/send",
+            type: "POST",
+            data: formData,
+            success: function (data) {
+                /*var status = JSON.parse(data).status*/
+                if (data.status == 201) {
+                    jQuery("#click_origin_subscriber").before('<div style="background-color: green;color: white;text-align: center;font-family: \'fonte-regular\';font-size: 13px;padding: 8px;border-radius: 9px;">ENTRAREMOS EM CONTATO O MAIS BREVE POSSÃVEL :)</div>');
+                } else {
+                    jQuery("#click_origin_subscriber").before('<div style="background-color:#F52929;color:white;text-align:center;font-family:\'fonte-regular\';">OCORREU UM ERRO TENTE NOVAMENTE</div>');
+                }
+                jQuery("#btn-submit-subscriber").prop("disabled", false);
+                clearFileds();
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                jQuery("#click_origin_subscriber").before('<div style="background-color:#F52929;color:white;text-align:center;font-family:\'fonte-regular\';">OCORREU UM ERRO TENTE NOVAMENTE</div>');
+            }
+        });
+    }
+});
--- wp-content/plugins/grumft-contact-form-plugin/core.php
+++ wp-content/plugins/grumft-contact-form-plugin/core.php
@@ -36,6 +36,31 @@ function send_register_form()
 	 echo $server_output;
 }
 
+function send_register_email_subscriber()
+{
+	
+	$email = $_POST["email"];
+	
+	$click_origin = $_POST["click_origin"];
+	$apiKey = "1234";
+
+	$json_obj = json_encode(array("email" => $email, 'click_origin' => $click_origin, 'apiKey' => $apiKey));
+
+//	echo $json_obj;
+
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, "https://admin.meediaonne.com/api/landing-page/subscriber/store");
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_USERAGENT, 'Mediapartner-SeaStorm-1.0');
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $json_obj);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$server_output = curl_exec($ch);
+	curl_close($ch);
+
+	 echo $server_output;
+}
 
 
 function prefix_register_send_form()
@@ -43,6 +68,10 @@ function prefix_register_send_form()
 	register_rest_route('register-form', '/send', array(
 		'methods'  => 'POST',
 		'callback' => 'send_register_form',
+	));
+	register_rest_route('email-subscriber', '/send', array(
+		'methods'  => 'POST',
+		'callback' => 'send_register_email_subscriber',
 	));
 }
 
