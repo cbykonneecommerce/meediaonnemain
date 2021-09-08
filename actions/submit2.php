97bd454f964c99b7787350806e4077f8ed66795d
usr_bazan <henrique.bazan86@gamil.com>
2021-09-08 15:56:17 -0300

    add campo investment

--- actions/submit2.php
+++ actions/submit2.php
@@ -5,24 +5,25 @@
 //		return false;
 //	}
 
-	$name = $_POST["name"];
-	$email = $_POST["email"];
-	$phone = $_POST["phone"];
-	$company = $_POST["company"];
-	$click_origin = $_POST["click_origin"];
-	$apiKey = "1234";
+$name = $_POST["name"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+$company = $_POST["company"];
+$click_origin = $_POST["click_origin"];
+$apiKey = "1234";
 
-	$json_obj = json_encode(array("name" => $name, "email" => $email, "phone" => $phone, "company" => $company, 'click_origin' => $click_origin, 'apiKey' => $apiKey));
+$json_obj = json_encode(array("name" => $name, "email" => $email, "phone" => $phone, "company" => $company, 'click_origin' => $click_origin, 'investment' => '', 'apiKey' => $apiKey));
 
 
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, "https://admin.meediaonne.com/api/landing-page/store");
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_USERAGENT, 'Mediapartner-SeaStorm-1.0');
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $json_obj);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	$server_output = curl_exec($ch);
-	curl_close($ch);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://admin.meediaonne.com/api/landing-page/store");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mediapartner-SeaStorm-1.0');
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+curl_setopt($ch, CURLOPT_POSTFIELDS, $json_obj);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$server_output = curl_exec($ch);
+curl_close($ch);
 
-	echo $server_output;
+
+echo $server_output;
