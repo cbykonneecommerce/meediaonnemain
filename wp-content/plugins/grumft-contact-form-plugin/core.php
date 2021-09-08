dc2ead14c5a59c307c8ee49b4c475a6da4f43df4
usr_bazan <henrique.bazan86@gamil.com>
2021-09-08 17:18:45 -0300

    teste erorr ajax

--- wp-content/plugins/grumft-contact-form-plugin/core.php
+++ wp-content/plugins/grumft-contact-form-plugin/core.php
@@ -20,18 +20,20 @@ function send_register_form()
 
 	$json_obj = json_encode(array("name" => $name, "email" => $email, "phone" => $phone, "company" => $company, 'click_origin' => $click_origin, 'investment' => $investment, 'apiKey' => $apiKey));
 
+	echo $json_obj;
 
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, "https://admin.meediaonne.com/api/landing-page/store");
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_USERAGENT, 'Mediapartner-SeaStorm-1.0');
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $json_obj);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	$server_output = curl_exec($ch);
-	curl_close($ch);
 
-	echo $server_output;
+	// $ch = curl_init();
+	// curl_setopt($ch, CURLOPT_URL, "https://admin.meediaonne.com/api/landing-page/store");
+	// curl_setopt($ch, CURLOPT_POST, 1);
+	// curl_setopt($ch, CURLOPT_USERAGENT, 'Mediapartner-SeaStorm-1.0');
+	// curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+	// curl_setopt($ch, CURLOPT_POSTFIELDS, $json_obj);
+	// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	// $server_output = curl_exec($ch);
+	// curl_close($ch);
+
+	// echo $server_output;
 }
 
 
